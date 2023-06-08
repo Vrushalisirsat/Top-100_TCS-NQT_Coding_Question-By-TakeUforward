eae7862e2388125a88c45c07ecff498d006e7917
Vrushali Sirsath <95585241+Vrushalisirsat@users.noreply.github.com>
2023-06-08 11:51:18 +0530

    update

--- equilibriumidx.java
+++ equilibriumidx.java
@@ -23,6 +23,14 @@ The sum of the numbers after index 2 is: 0
 
 */
 
+/*
+Solution 1: Using Two Loops(Brute Force)
+Consider two variables leftSum and rightSum initialized to zero.Now for every index, i calculate the leftSum till that index and rightSum till that index.
+At any point if (leftSum == rightSum) return that index i.
+If equilibrium index not found return -1.
+*/
+
+/*
 import java.util.*;
 class equilibriumidx {
 
@@ -53,6 +61,64 @@ class equilibriumidx {
       return -1;
      
     }
+   public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array : ");
+        int n = sc.nextInt();
+        System.out.println("Enter the array Element : ");
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int equilibriumidx = findEquilibriumIdx(arr,n);
+        System.out.println(equilibriumidx);
+       
+    }
+}
+*/
+/*
+
+Solution 2: Using Total Sum
+Calculate the sum = total sum of all the integers in the array.
+Keep leftSum = 0, rightSum = sum.
+leftSum = sum of all the integers to its left 
+rightSum = sum of all the integers to it’s right.
+At every index i rightSum would be rightSum excluding the current index value.now we will is Check if(leftSum == rightSum) If yes then return that index else keep moving forward.while moving forward it can be seen that we are considering that current index value to be on left so update the leftSum value. leftSum = leftSum + nums[i].
+If no such index is found return -1.
+
+*/
+
+import java.util.*;
+class equilibriumidx {
+
+    static int findEquilibriumIdx(int arr[] ,int n)
+    {
+      int totalSum = 0;
+      for(int i=0;i<n;i++)
+      {
+        totalSum += arr[i];
+      }
+      
+      int leftSum=0,rightSum=totalSum;
+
+      for(int i=0;i<n;i++)
+      {
+        rightSum -= arr[i];
+
+      if(leftSum == rightSum)
+      {
+        return i;
+      }
+
+      leftSum += arr[i];
+      }
+      return -1;
+  }
+      
+     
    public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
